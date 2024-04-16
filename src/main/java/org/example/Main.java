960e572a872edcaf9bde70c457b88359cdf8f78b
abdelrhman <115238439+abdelrhman892@users.noreply.github.com>
2024-04-16 19:03:28 +0200

    quick sort

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,8 @@
 package org.example;
 
 public class Main {
-    public static void selectionSort(int[] arr){
+
+    private static void selectionSort(int[] arr){
         for (int i = 0; i < arr.length; i++){
             for (int j = i+1; j < arr.length; j++){
                 if (arr[j] < arr[i]){
@@ -14,7 +15,7 @@ public class Main {
         }
     }
 
-    public static void bubbleSort(int[] arr){
+    private static void bubbleSort(int[] arr){
         for (int i = 0; i < arr.length - 1; i++){
             for (int j = 0; j < arr.length - 1 - i; j++){
                 if (arr[j] > arr[j + 1]){
@@ -26,7 +27,7 @@ public class Main {
         }
     }
 
-    public static void insertionSort(int[] arr){
+    private static void insertionSort(int[] arr){
         for (int i = 1; i < arr.length; i++){
             int temp = arr[i];
             int j = i - 1;
@@ -93,12 +94,42 @@ public class Main {
         }
     }
 
+    private static void quickSort(int[] array, int start, int end) {
+        if (end <= start) return;
+
+        int pivot = partition(array, start, end);
+        quickSort(array, start, pivot - 1);
+        quickSort(array, pivot + 1, end);
+    }
+
+    private static int partition(int[] array, int start, int end) {
+        int pivot =  array[end];
+        int i = start - 1;
+        for (int j = start; j < end; j++){
+            if (array[j] < pivot){
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        i++;
+        int temp = array[i];
+        array[i] = array[end];
+        array[end] = temp;
+
+        return i;
+    }
+
 
     public static void main(String[] args) {
-        int[] nums = {9, 1, 8, 2, 7, 3, 6, 5, 4};
-        selectionSort(nums);
-        for (int num : nums){
-            System.out.print(num + " ");
+        int[] array = {8, 2, 5, 3, 9, 4, 7, 6, 1};
+        // Call the sort function you want
+        for (int i : array){
+            System.out.print(i);
         }
+
     }
+
+
 }
